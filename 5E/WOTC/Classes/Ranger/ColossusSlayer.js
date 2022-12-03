d4199a2063475a8d8d34d47547a53a200f89313f
sasquach45932 <92884040+sasquach45932@users.noreply.github.com>
2022-12-03 18:46:27 +0100

    Add files via upload

--- 5E/WOTC/Classes/Ranger/ColossusSlayer.js
+++ 5E/WOTC/Classes/Ranger/ColossusSlayer.js
@@ -1,18 +1,18 @@
 // Special thanks to thatlonelybugbear for the original macro
 // Requires the following modules: Dynamic Active Effects, Item Macros, Times-Up, About-Time
-// Macro version 1.00 // 20220323
+// Macro version 1.01 // 20221118 For Foundry V10
 
-
-if (!["mwak","rwak"].includes(args[0].item.data.actionType)) return {};         
-if (args[0].hitTargets.length < 1) return {};                                   
+if (!["mwak","rwak"].includes(args[0].item.system.actionType)) return {};
+if (args[0].hitTargets.length < 1) return {};
 token = canvas.tokens.get(args[0].tokenId);
 actor = token.actor;
 if (!actor || !token || args[0].hitTargets.length < 1) return {};
 
 let target = canvas.tokens.get(args[0].hitTargets[0].id);
 //checking hp max versus hp current value on the target and if they are not different just stop, otherwise go on to return the extra damage
-let hpmax = target.actor.data.data.attributes.hp.max;                                    
-let hpval = target.actor.data.data.attributes.hp.value;
+let hpmax = target.actor.system.attributes.hp.max;
+let hpval = target.actor.system.attributes.hp.value;
+
 if (hpmax === hpval) return {};
 
 const diceMult = args[0].isCritical ? 2: 1;
