48131046728ae3e10c48c601c5b5dbbc5aed17e0
sasquach45932 <92884040+sasquach45932@users.noreply.github.com>
2022-12-03 19:20:55 +0100

    Add files via upload

--- 5E/WOTC/Spells/Cantrip/ShockingGrasp.js
+++ 5E/WOTC/Spells/Cantrip/ShockingGrasp.js
@@ -1,14 +1,14 @@
 // Requires the following modules: Item Macros, MidiQOL
 // Trigger Item macro by "Before Attack roll"
-// Macro version 1.0 // 20220327
+// Macro version 1.01 // 20221128 For FoundryVTT V10
 
 let targets = args[0].targets[0];
 let target = await canvas.tokens.get(targets.id);
 
-if (target.actor.data.data.attributes.ac.equippedArmor) {
-    let armor = target.actor.data.data.attributes.ac.equippedArmor.data._id;
-    const armorI = target.actor.data.items.find(i => i.data._id == armor);
-    let armorIDesc = armorI.data.data.description.value.toLowerCase();
+if (target.actor.system.attributes.ac.equippedArmor) {
+    let armor = target.actor.system.attributes.ac.equippedArmor._id;
+    const armorI = target.actor.items.find(i => i._id == armor);
+    let armorIDesc = armorI.system.description.value.toLowerCase();
     let pos = armorIDesc.search("metal");
     if (pos >= 0) {
         const workflow = MidiQOL.Workflow.getWorkflow(args[0].uuid)
--- 5E/WOTC/Spells/Cantrip/TollTheDead.js
+++ 5E/WOTC/Spells/Cantrip/TollTheDead.js
@@ -1,16 +1,17 @@
 // Original script: https://github.com/Maselkov/masels-foundryvtt-macros/blob/master/spells/0/toll-the-dead.js
 // Requires the following modules: Item Macros, MidiQOL
-// Macro version 1.00 // 20220323
+// Macro version 1.01 // 20220323 For FoundryVTT V10
+
 if (!args[0].failedSaves.length) {
     return;
 }
 
 let targets = args[0].targets[0];
 let target = await canvas.tokens.get(targets.id);
-let level = actor.data.data.details.level;
+let level = actor.system.details.level;
 let numDice = 1 + Math.floor((level + 1) / 6);
 
-let damageRoll = target.actor.data.data.attributes.hp.max != target.actor.data.data.attributes.hp.value ? new Roll(`${numDice}d12`).evaluate({
+let damageRoll = target.actor.system.attributes.hp.max != target.actor.system.attributes.hp.value ? new Roll(`${numDice}d12`).evaluate({
     async: false
 }) : new Roll(`${numDice}d8`).evaluate({
     async: false
